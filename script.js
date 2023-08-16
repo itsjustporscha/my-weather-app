aa1b239379337c245e050880a8050ba60f44b609
Porscha <itsjustporscha@gmail.com>
2023-08-16 13:12:42 -0400

    added updates

--- script.js
+++ script.js
@@ -79,8 +79,8 @@ function displayCurrentTemp(response) {
 }
 
 function currentPingLocation(position) {
-  let apiKey = "t016074fb65443606obfa1c77473d7be";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
+  let apiKey = "48bfbdd47439302d4823e096b598aab9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
   console.log(apiUrl);
 
   axios.get(apiUrl).then(displayWeather);
